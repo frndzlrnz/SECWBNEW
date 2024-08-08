3ccc97b1ae2a7160ce019b2ce8810efcb2605a65
Syirhel Jera <108814078+SyirhelJera@users.noreply.github.com>
2024-08-09 01:27:57 +0800

    Login Logs, Recaptcha Update

--- login.php
+++ login.php
@@ -1,9 +1,10 @@
+<!DOCTYPE html>
 <html>
 <head>
     <title>Login</title>
     <link rel="stylesheet" href="login_style.css">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
-
 <body>
     <div class="login"><center>
         <h1>Login</h1>
@@ -16,10 +17,10 @@
                 <input type="password" name="password" class="pswd" placeholder="Enter Password" size="100%" required>
             </div>
 
+            <div class="g-recaptcha" data-sitekey="6LdpOPMpAAAAAJ_QkvPEDLdd32FyZMknQMsnj_E8"></div>
             <p><input type="submit" class="btn" value="Login" name="submitbtn"></p>
         </form><br>
         <a href="welcome.php"><button class="btn">Back</button></a><br><br>
     </center></div>
 </body>
-
 </html>
